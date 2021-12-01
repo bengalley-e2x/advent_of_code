b78f60a7f3a26953b9be38e611fb74213481414b
ben.galley <benjamin.galley@e2x.com>
2021-12-01 13:42:43 +0000

    add test for a longer input array

--- spec/day_1_spec.rb
+++ spec/day_1_spec.rb
@@ -10,5 +10,10 @@ describe "countHigher" do
     input = [2, 1]
     expect(countHigher(input)).to eq(0)
   end 
+
+  it "counts 1 when input increases first up, then down" do
+    input = [1, 2, 1]
+    expect(countHigher(input)).to eq(1)
+  end  
 end    
 
