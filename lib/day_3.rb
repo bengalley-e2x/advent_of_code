1d38b120288a17e681e30085df0868a31282974e
ben.galley <benjamin.galley@e2x.com>
2021-12-07 19:21:29 +0000

    day 3 part 1

--- lib/day_3.rb
+++ lib/day_3.rb
@@ -1,30 +1,49 @@
 data = File.readlines('input_data/day_3_input.txt')
 
-puts data[0]
-
-def gamma_rate(input)
-  ones = 0
-  zeros = 0
-  input.each do | number |
-    strings = number
-    bits = strings.chars
-    puts "bits: #{bits}" 
-    if bits[0] == "0"
-      zeros += 1
-      puts "zeros: #{zeros}"
-    elsif bits[0] == "1"
-      ones +=1
-      puts "ones: #{ones}"
-    end
-  end
-  ones > zeros ? 1 : 0
+def gamma(input)
+  gamma_rate = []
+  bit_count = input[0].length
+  for i in 1..bit_count
+    ones = 0
+    zeros = 0
+    input.each do | number | 
+      if number[i-1] == "0"
+        zeros += 1
+      elsif number[i-1] == "1"
+        ones += 1  
+      end    
+    end 
+    if ones > zeros
+      gamma_rate[i-1] = "1"
+    elsif zeros > ones
+      gamma_rate[i-1] = "0" 
+    end  
+  end  
+  puts "gamma: #{gamma_rate.join}"   
+  gamma_rate.join
 end  
 
-# bit_count = input.length
-# for bit in 0..bit_count do
-# bits = number.chars.to_s.map do | bit | 
-#   bit.to_i
-# end  
+def epsilon(input)
+  epsilon = []
+  input.chars.each_with_index do | digit, index |
+    if digit == "0"
+      epsilon[index] = "1"
+    elsif digit == "1"
+      epsilon[index] = "0"
+    end
+  end 
+  puts "epsilon: #{epsilon.join}"
+  epsilon.join     
+end  
 
+def power_consumption(gamma, epsilon)
+  power = gamma.to_i(2) * epsilon.to_i(2)
+  puts "power: #{power}"
+  power
+end  
 
+gamma = gamma(data)
+epsilon = epsilon(gamma)
+
+power_consumption(gamma, epsilon)
 
--- spec/day_3_spec.rb
+++ spec/day_3_spec.rb
@@ -2,8 +2,19 @@ require "day_3"
 
 test_data = ["10101", "00011", "10011"]
 describe "gamma_rate" do
-  it "calculates the most common first bit" do
-    expect(gamma_rate(test_data)).to eq(1)
+  it "calculates the gamma rate" do
+    expect(gamma(test_data)).to eq("10011")
   end
+
+  it "calculates the epsilon rate" do
+    gamma_rate = gamma(test_data)
+    expect(epsilon(gamma_rate)).to eq("01100")
+  end
+
+  it "calculates the power consumption" do
+    gamma_rate = gamma(test_data)
+    epsilon_rate = epsilon(gamma_rate)
+    expect(power_consumption(gamma_rate, epsilon_rate)).to eq(228)
+  end  
 end
 
